3ef77a961c3ea3414ea6a2aa3685d4dfe2f1296a
a.pcozar <a.pcozar@udc.es>
2020-10-10 15:22:12 +0200

    SaleAddException added

---
+++ src/main/java/es/udc/fi/dc/service/exceptions/SaleAddServiceException.java
@@ -0,0 +1,10 @@
+package es.udc.fi.dc.service.exceptions;
+
+public class SaleAddServiceException extends Exception {
+	private static final long serialVersionUID = -1544036070769130658L;
+
+	public SaleAddServiceException(String errorMessage) {
+		super(errorMessage);
+	}
+
+}
